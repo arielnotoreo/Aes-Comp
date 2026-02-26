12aeeae2cceadec315c6a90f9ff0179720293ed7
Ariel Enzhu Cthwe <arielcthwe@gmail.com>
2026-02-26 02:53:24 -0500

    done </3

--- VeraMolnarRecode/VeraMolnarRecode/VeraMolnarRecode.cpp
+++ VeraMolnarRecode/VeraMolnarRecode/VeraMolnarRecode.cpp
@@ -1,23 +1,39 @@
-// VeraMolnarRecode.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include "simple_svg_1.0.0.hpp"
+#include <cstdlib>
+#include <ctime>
 
-using namespace svg;
+using namespace std;
 
-int main()
+int main() 
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+
+    const int rows = 20;
+    const int cols = 40;
+
+    for (int y = 0; y < rows; y++) 
+    {
+        for (int x = 0; x < cols; x++) 
+        {
+            // random interuption
+            if (rand() % 100 < 15) 
+            {
+                cout << "  ";   // blank space
+                continue;
+            }
+
+            // chosing the line type to give the rotation effect
+            int r = rand() % 4;
+
+            // 4 differnt lines to choose from
+            if (r == 0) cout << "--";
+            else if (r == 1) cout << "| ";
+            else if (r == 2) cout << "/ ";
+            else cout << "\\ ";
+
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
